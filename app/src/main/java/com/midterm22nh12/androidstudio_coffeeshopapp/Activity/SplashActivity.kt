2bb2b7de27a8e0e7b7abd2cf6daea7f35e068b6d
quynhnhi <nhii2208@gmail.com>
2025-04-11 16:45:36 +0700

    first commit

---
+++ app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.midterm22nh12.androidstudio_coffeeshopapp.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.midterm22nh12.androidstudio_coffeeshopapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

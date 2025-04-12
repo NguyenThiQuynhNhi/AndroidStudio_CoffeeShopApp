84893085b242b70d0d1e56040666be4a53dc053b
Ngoc <dbhngoc1307@gmail.com>
2025-04-12 21:12:59 +0700

    LTDĐ - Code tam thoi

--- app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/SplashActivity.kt
+++ app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/SplashActivity.kt
@@ -6,12 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.midterm22nh12.androidstudio_coffeeshopapp.R
+import com.midterm22nh12.androidstudio_coffeeshopapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        binding=ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener{
+
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

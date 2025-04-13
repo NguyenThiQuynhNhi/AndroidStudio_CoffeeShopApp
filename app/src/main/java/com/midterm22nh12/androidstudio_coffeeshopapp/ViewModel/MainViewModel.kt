e9f178dcc945079c5889d1334c27688e10c0e595
Ngoc <dbhngoc1307@gmail.com>
2025-04-13 13:13:33 +0700

    LTDD - Dashboard Backend

--- app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/ViewModel/MainViewModel.kt
@@ -3,22 +3,12 @@ package com.midterm22nh12.androidstudio_coffeeshopapp.ViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.midterm22nh12.androidstudio_coffeeshopapp.Domain.BannerModel
-import com.midterm22nh12.androidstudio_coffeeshopapp.Domain.ItemsModel
 import com.midterm22nh12.androidstudio_coffeeshopapp.Repository.MainRepository
-import com.midterm22nh12.androidstudio_coffeeshopapp.com.midterm22nh12.androidstudio_coffeeshopapp.Domain.CategoryModel
 
 class MainViewModel: ViewModel() {
     private val repository=MainRepository()
 
     fun loadBanner():LiveData<MutableList<BannerModel>>{
         return repository.loadBanner()
-    }
-
-    fun loadCategory():LiveData<MutableList<CategoryModel>>{
-        return repository.loadCategory()
-    }
-
-    fun loadPopular():LiveData<MutableList<ItemsModel>>{
-        return repository.loadPopular()
     }
 }

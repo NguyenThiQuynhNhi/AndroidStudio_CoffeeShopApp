12f31c6c6653037b835a5c57b84e198172a54475
Ngoc <dbhngoc1307@gmail.com>
2025-04-13 18:13:11 +0700

    LTDD - Dashboard Backend 2

--- app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/MainActivity.kt
+++ app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.midterm22nh12.androidstudio_coffeeshopapp
+package com.midterm22nh12.androidstudio_coffeeshopapp.com.midterm22nh12.androidstudio_coffeeshopapp.Activity
 
 import android.os.Bundle
 import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.midterm22nh12.androidstudio_coffeeshopapp.Adapter.CategoryAdapter
 import com.midterm22nh12.androidstudio_coffeeshopapp.databinding.ActivityMainBinding
 import com.midterm22nh12.androidstudio_coffeeshopapp.ViewModel.MainViewModel
 
@@ -17,6 +19,7 @@ class MainActivity : ComponentActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initBanner()
+        initcategory()
     }
 
     private fun initBanner() {
@@ -30,5 +33,21 @@ class MainActivity : ComponentActivity() {
             }
             binding.progressBarBanner.visibility = View.GONE
         }
+        viewModel.loadBanner()
+    }
+
+    private fun initcategory(){
+        binding.progressBarCatagory.visibility=View.VISIBLE
+        viewModel.loadCategory().observeForever {
+            binding.recyclerViewCat.layoutManager=
+                LinearLayoutManager(this@MainActivity,
+                    LinearLayoutManager.HORIZONTAL,
+                    false
+                )
+
+            binding.recyclerViewCat.adapter=CategoryAdapter(it)
+            binding.progressBarCatagory.visibility=View.GONE
+        }
+        viewModel.loadCategory()
     }
 }
--- app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/ViewModel/MainViewModel.kt
@@ -4,11 +4,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.midterm22nh12.androidstudio_coffeeshopapp.Domain.BannerModel
 import com.midterm22nh12.androidstudio_coffeeshopapp.Repository.MainRepository
+import com.midterm22nh12.androidstudio_coffeeshopapp.com.midterm22nh12.androidstudio_coffeeshopapp.Domain.CategoryModel
 
 class MainViewModel: ViewModel() {
     private val repository=MainRepository()
 
     fun loadBanner():LiveData<MutableList<BannerModel>>{
         return repository.loadBanner()
+    }
+
+    fun loadCategory():LiveData<MutableList<CategoryModel>>{
+        return repository.loadCategory()
     }
 }

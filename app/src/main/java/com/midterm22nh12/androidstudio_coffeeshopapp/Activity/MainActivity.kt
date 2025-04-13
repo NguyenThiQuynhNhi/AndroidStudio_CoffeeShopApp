9c34beebea5a318b6b28cb3693e6b4fed37515de
Ngoc <dbhngoc1307@gmail.com>
2025-04-13 18:13:11 +0700

    LTDD - Dashboard Backend 2

--- app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/MainActivity.kt
+++ app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/MainActivity.kt
@@ -1,90 +1,53 @@
-package com.midterm22nh12.androidstudio_coffeeshopapp.Activity
+package com.midterm22nh12.androidstudio_coffeeshopapp.com.midterm22nh12.androidstudio_coffeeshopapp.Activity
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.midterm22nh12.androidstudio_coffeeshopapp.Adapter.CategoryAdapter
-import com.midterm22nh12.androidstudio_coffeeshopapp.Adapter.AllCoffeeAdapter
-import com.midterm22nh12.androidstudio_coffeeshopapp.R
 import com.midterm22nh12.androidstudio_coffeeshopapp.databinding.ActivityMainBinding
 import com.midterm22nh12.androidstudio_coffeeshopapp.ViewModel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel=MainViewModel()
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initBanner()
         initcategory()
-        initAllCoffee()
-        initBottomMenu()
-    }
-
-    private fun initBottomMenu() {
-        binding.cartBtn.setOnClickListener {
-            startActivity(Intent(this, CartActivity::class.java))
-        }
-        binding.favorBtn.setOnClickListener({
-//          triển khai FavoriteActivity thì mở comment
-//          startActivity(Intent(this, FavoriteActivity::class.java))
-        })
-        binding.orderBtn.setOnClickListener{
-            startActivity(Intent(this, MyOrderActivity::class.java))
-        }
-        binding.profileBtn.setOnClickListener{
-            startActivity(Intent(this, ProfileActivity::class.java))
-        }
     }
 
     private fun initBanner() {
         binding.progressBarBanner.visibility = View.VISIBLE
-
-        viewModel.loadBanner().observeForever { list ->
-            val bannerItem = list?.firstOrNull()
-
-            if (bannerItem != null && !bannerItem.url.isNullOrEmpty()) {
-                Glide.with(binding.banner.context)
-                    .load(bannerItem.url)
+        viewModel.loadBanner().observe(this@MainActivity) { banners ->
+            if (banners.isNotEmpty() && !banners[0].url.isNullOrEmpty()) {
+                Glide.with(this@MainActivity)
+                    .load(banners[0].url)
                     .into(binding.banner)
             } else {
-                Glide.with(binding.banner.context)
-                    .load(R.drawable.banner)
-                    .into(binding.banner)
             }
-
             binding.progressBarBanner.visibility = View.GONE
         }
+        viewModel.loadBanner()
     }
 
     private fun initcategory(){
-        binding.progressBarCategory.visibility=View.VISIBLE
+        binding.progressBarCatagory.visibility=View.VISIBLE
         viewModel.loadCategory().observeForever {
-            binding.recyclerViewCategory.layoutManager=
+            binding.recyclerViewCat.layoutManager=
                 LinearLayoutManager(this@MainActivity,
                     LinearLayoutManager.HORIZONTAL,
                     false
                 )
 
-            binding.recyclerViewCategory.adapter= CategoryAdapter(it)
-            binding.progressBarCategory.visibility=View.GONE
+            binding.recyclerViewCat.adapter=CategoryAdapter(it)
+            binding.progressBarCatagory.visibility=View.GONE
         }
-    }
-
-    private fun initAllCoffee() {
-        binding.progressBarPopular.visibility = View.VISIBLE
-        viewModel.loadAllCoffee().observeForever {
-            binding.recyclerViewPopular.layoutManager = GridLayoutManager(this, 2)
-            binding.recyclerViewPopular.adapter = AllCoffeeAdapter(it.toMutableList())
-            binding.progressBarPopular.visibility = View.GONE
-        }
+        viewModel.loadCategory()
     }
 }

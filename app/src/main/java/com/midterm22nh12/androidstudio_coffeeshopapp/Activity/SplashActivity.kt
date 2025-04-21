d5b9ff6d9cd400d6810d3dcfd9b0b07cab952820
Ngoc <dbhngoc1307@gmail.com>
2025-04-21 08:37:58 +0700

    LTDĐ - Item Detail UI

--- app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/SplashActivity.kt
+++ app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/SplashActivity.kt
@@ -1,11 +1,9 @@
 package com.midterm22nh12.androidstudio_coffeeshopapp.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.midterm22nh12.androidstudio_coffeeshopapp.R
 import com.midterm22nh12.androidstudio_coffeeshopapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -18,13 +16,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.startBtn.setOnClickListener{
-
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }

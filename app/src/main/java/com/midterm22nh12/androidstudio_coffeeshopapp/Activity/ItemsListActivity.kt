8b02fbfe80b023ece66ec6a9300ed6aeabf1dfb6
quynhnhi <nhii2208@gmail.com>
2025-05-01 00:06:19 +0700

    List of Items Backend: completed

--- app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/ItemsListActivity.kt
+++ app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/ItemsListActivity.kt
@@ -1,21 +1,54 @@
 package com.midterm22nh12.androidstudio_coffeeshopapp.Activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.midterm22nh12.androidstudio_coffeeshopapp.Adapter.ItemsListCategoryAdapter
 import com.midterm22nh12.androidstudio_coffeeshopapp.R
+import com.midterm22nh12.androidstudio_coffeeshopapp.ViewModel.MainViewModel
+import com.midterm22nh12.androidstudio_coffeeshopapp.databinding.ActivityItemsListBinding
+import com.midterm22nh12.androidstudio_coffeeshopapp.databinding.ViewholderItemPicLeftBinding
 
 class ItemsListActivity : AppCompatActivity() {
+    lateinit var binding:ActivityItemsListBinding
+    private val viewModel = MainViewModel()
+    private var id:String=""
+    private var title:String=""
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_items_list)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityItemsListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getBundle()
+        initList()
+    }
+
+    private fun initList() {
+        binding.apply {
+            progressBar.visibility = View.VISIBLE
+            viewModel.loadItems(id).observe(this@ItemsListActivity, Observer {
+                listView.layoutManager =
+                    LinearLayoutManager(this@ItemsListActivity,
+                        LinearLayoutManager.VERTICAL, false)
+                listView.adapter = ItemsListCategoryAdapter(it)
+                progressBar.visibility = View.GONE
+            })
+            backBtn.setOnClickListener { finish() }
         }
+    }
+
+    private fun getBundle() {
+        id = intent.getStringExtra("id")!!
+        title = intent.getStringExtra("title")!!
+
+        binding.categoryTxt.text = title
     }
 }

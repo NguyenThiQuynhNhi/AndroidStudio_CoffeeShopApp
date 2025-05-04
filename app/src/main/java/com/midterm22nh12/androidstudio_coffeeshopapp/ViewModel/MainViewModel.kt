ad14019fd616040a4234be0e90d5f30c09eda4cf
quynhnhi <nhii2208@gmail.com>
2025-05-04 10:04:22 +0700

    fix UI & Backend
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/DetailActivity.kt
    #	app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/MainActivity.kt
    #	app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/SplashActivity.kt
    #	app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Repository/MainRepository.kt
    #	app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/ViewModel/MainViewModel.kt
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/ViewModel/MainViewModel.kt
@@ -17,8 +17,8 @@ class MainViewModel: ViewModel() {
         return repository.loadCategory()
     }
 
-    fun loadPopular():LiveData<MutableList<ItemsModel>>{
-        return repository.loadPopular()
+    fun loadAllCoffee():LiveData<MutableList<ItemsModel>>{
+        return repository.loadAllCoffee()
     }
 
     fun loadItems(categoryId:String):LiveData<MutableList<ItemsModel>>{

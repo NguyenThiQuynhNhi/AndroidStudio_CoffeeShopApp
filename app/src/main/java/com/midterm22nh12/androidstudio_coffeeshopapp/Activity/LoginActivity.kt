ee81c9acb494b426eff685add65ba2d92d423e6a
quynhnhi <nhii2208@gmail.com>
2025-05-11 00:15:51 +0700

    Login-Signup-ForgotPass UI & Backend :completed

--- app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/LoginActivity.kt
+++ app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/LoginActivity.kt
@@ -1,7 +1,6 @@
 package com.midterm22nh12.androidstudio_coffeeshopapp.Activity
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -59,20 +58,12 @@ class LoginActivity : AppCompatActivity() {
             auth.signInWithEmailAndPassword(email, pass)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        // ✅ Lưu email vào SharedPreferences
-                        val prefs = getSharedPreferences("UserInfo", Context.MODE_PRIVATE)
-                        prefs.edit().apply {
-                            putString("email", email)
-                            apply()
-                        }
-
                         startActivity(Intent(this, MainActivity::class.java))
                         finish()
                     } else {
                         Toast.makeText(this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show()
                     }
                 }
-
         }
 
 
@@ -134,22 +125,9 @@ class LoginActivity : AppCompatActivity() {
                     val task: Task<GoogleSignInAccount> =
                         GoogleSignIn.getSignedInAccountFromIntent(data)
                     try {
-                        val account = task.getResult(ApiException::class.java)
-                        val name = account.displayName
-                        val email = account.email
-
-                        // Lưu vào SharedPreferences để ProfileActivity dùng
-                        val prefs = getSharedPreferences("UserInfo", Context.MODE_PRIVATE)
-                        prefs.edit().apply {
-                            putString("name", name)
-                            putString("email", email) // ✅ Dùng đúng key là "email"
-                            apply()
-                        }
-
-
+                        task.getResult(ApiException::class.java)
                         finish()
                         startActivity(Intent(this, MainActivity::class.java))
-
                     } catch (e: ApiException) {
                         Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
                     }

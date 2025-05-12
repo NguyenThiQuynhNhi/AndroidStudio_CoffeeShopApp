992e2abedf5781f8373ef4d4561de3fc645a0913
Ngoc <dbhngoc1307@gmail.com>
2025-05-12 14:42:16 +0700

    LTDĐ - Cart + Profile + Order

--- app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/LoginActivity.kt
+++ app/src/main/java/com/midterm22nh12/androidstudio_coffeeshopapp/Activity/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.midterm22nh12.androidstudio_coffeeshopapp.Activity
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -125,9 +126,21 @@ class LoginActivity : AppCompatActivity() {
                     val task: Task<GoogleSignInAccount> =
                         GoogleSignIn.getSignedInAccountFromIntent(data)
                     try {
-                        task.getResult(ApiException::class.java)
+                        val account = task.getResult(ApiException::class.java)
+                        val name = account.displayName
+                        val email = account.email
+
+                        // Lưu vào SharedPreferences để ProfileActivity dùng
+                        val prefs = getSharedPreferences("UserInfo", Context.MODE_PRIVATE)
+                        prefs.edit().apply {
+                            putString("name", name)
+                            putString("phone", email) // Hoặc tạo thêm key "email"
+                            apply()
+                        }
+
                         finish()
                         startActivity(Intent(this, MainActivity::class.java))
+
                     } catch (e: ApiException) {
                         Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
                     }
